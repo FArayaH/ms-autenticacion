7a22ec64843a8be355bdc2a6e2216616a978f220
Felipe <felipeandreshenriquez2004@gmail.com>
2026-05-14 16:03:11 -0400

    modificacion de la clase JtwService

--- src/main/java/com/hotel/ms_autenticacion/service/JwtService.java
+++ src/main/java/com/hotel/ms_autenticacion/service/JwtService.java
@@ -3,14 +3,18 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
 @Service
 public class JwtService {
-    // Clave secreta fija para compartir con los otros microservicios
-    private static final String SECRET_KEY_STRING = "MiClaveSecretaSuperSeguraParaHotel1234567890";
-    private static final Key SECRET_KEY = Keys.hmacShaKeyFor(SECRET_KEY_STRING.getBytes());
+
+    // 1. Contraseña fija y larga (mínimo 32 caracteres para seguridad HS256)
+    private static final String SECRET = "hotel-microservicios-secret-key-2026-segura";
+
+    // 2. Clave generada a partir de la contraseña fija (Ya no es aleatoria)
+    private static final Key SECRET_KEY = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
 
     // Tiempo de expiración del token: 1 hora (3600000 milisegundos)
     private static final long EXPIRATION_TIME = 3600000;
@@ -18,10 +22,10 @@ public class JwtService {
     public String generarToken(String username, String role) {
         return Jwts.builder()
                 .setSubject(username)
-                .claim("role", role) // Añadimos el rol del usuario como un "Claim"
+                .claim("role", role)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SECRET_KEY) // Firmamos el token con nuestra clave secreta
+                .signWith(SECRET_KEY)
                 .compact();
     }
 }
